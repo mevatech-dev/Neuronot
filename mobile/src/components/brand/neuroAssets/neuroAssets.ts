fb8b168adfb6c5417e92f97c26b214d01a646ce4
aa <info@mevatech.dev>
2026-05-08 17:59:46 +0300

    feat: update app.json dependencies, add .nvmrc and .gitignore files

--- mobile/src/components/brand/neuroAssets/neuroAssets.ts
+++ mobile/src/components/brand/neuroAssets/neuroAssets.ts
@@ -1,12 +1,12 @@
 import type { ImageSourcePropType } from 'react-native';
 
 export const neuroMoods = {
-  calm: require('../../../assets/images/neuro-calm.png'),
-  happy: require('../../../assets/images/neuro-happy.png'),
-  thinking: require('../../../assets/images/neuro-thinking.png'),
-  encouraging: require('../../../assets/images/neuro-encouraging.png'),
-  sleepy: require('../../../assets/images/neuro-sleepy.png'),
-  sad: require('../../../assets/images/neuro-sad.png'),
+  calm: require('../../../../assets/images/neuro-calm.png'),
+  happy: require('../../../../assets/images/neuro-happy.png'),
+  thinking: require('../../../../assets/images/neuro-thinking.png'),
+  encouraging: require('../../../../assets/images/neuro-encouraging.png'),
+  sleepy: require('../../../../assets/images/neuro-sleepy.png'),
+  sad: require('../../../../assets/images/neuro-sad.png'),
 } satisfies Record<string, ImageSourcePropType>;
 
 export type NeuroMood = keyof typeof neuroMoods;
